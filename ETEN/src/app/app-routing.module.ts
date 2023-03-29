74ac3cccd86b3f6e88db66ebfeee608c31d5baa5
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-03-29 13:51:15 +0200

    Buscardor por categoria

--- ETEN/src/app/app-routing.module.ts
+++ ETEN/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AboutUsComponent } from './views/about-us/about-us.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { SignUpComponent } from './views/sign-up/sign-up.component';
+import { BuscarCategoriaComponent } from './views/buscar-categoria/buscar-categoria.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'index', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'sign-up', component:SignUpComponent},
-  {path: 'about-us', component:AboutUsComponent}];
+  {path: 'about-us', component:AboutUsComponent},
+  {path: 'buscar-categoria', component:BuscarCategoriaComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ETEN/src/app/app.module.ts
+++ ETEN/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SignUpComponent } from './views/sign-up/sign-up.component';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutUsComponent } from './views/about-us/about-us.component';
+import { BuscarCategoriaComponent } from './views/buscar-categoria/buscar-categoria.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AboutUsComponent } from './views/about-us/about-us.component';
     LoginComponent,
     SignUpComponent,
     AboutUsComponent,
+    BuscarCategoriaComponent,
 
   ],
   imports: [

4fad8c19606c13d705e982c458d16f0d099583d2
Roberto Echevarria Saiz <rober.eche.sa@gmail.com>
2023-04-06 13:53:23 +0200

    top del perfil responsive y modificacion de img user

--- ETEN/src/app/views/perfil/perfil.component.ts
+++ ETEN/src/app/views/perfil/perfil.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class PerfilComponent {
 
+
+  public imagenSeleccionada: string = 'https://cdn-icons-png.flaticon.com/512/747/747376.png';
+
+  public seleccionarImagen(): void {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.onchange = (event: Event) => {
+      const inputElement = event.target as HTMLInputElement;
+      if (!inputElement || !inputElement.files || !inputElement.files[0]) {
+        return;
+      }
+      const archivoSeleccionado = inputElement.files[0];
+      const lector = new FileReader();
+      lector.readAsDataURL(archivoSeleccionado);
+      lector.onload = () => {
+        if (lector.result != null) {
+          this.imagenSeleccionada = lector.result.toString();
+        }
+      };
+    };
+    input.click();
+  }
+
+
 }

15e464efeb7ca54ab7945c6ace83d10167fa978e
Lian Salmerón López <lembajosa@gmail.com>
2023-04-07 22:44:11 +0200

    Función para comprobar el registro

--- ETEN/src/app/views/sign-up/sign-up.component.ts
+++ ETEN/src/app/views/sign-up/sign-up.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
+
+
+
 export class SignUpComponent {
 
+    expresionEmail: RegExp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    nombreUsuario:string = '';
+    emailUsuario:string = '';
+    emailConfirm:string = '';
+    contraseniaUsuario:string = '';
+    contraseniaConfirm:string = '';
+
+    // funcion que recoge la informacion del registro, comprueba que no haya campos vacios y que los emails y
+    // contraseñas coincidan y que el formato del email sea correcto para poder registrarse
+    public obtenerDatosRegistro() {
+
+        this.nombreUsuario = (<HTMLInputElement>document.getElementById('registro_nombre')).value;
+        this.emailUsuario = (<HTMLInputElement>document.getElementById('registro_email')).value;
+        this.emailConfirm = (<HTMLInputElement>document.getElementById('registro_confirme_email')).value;
+        this.contraseniaUsuario = (<HTMLInputElement>document.getElementById('registro_password')).value;
+        this.contraseniaConfirm = (<HTMLInputElement>document.getElementById('registro_confirme_password')).value;
+
+        if (this.nombreUsuario == "" || this.emailUsuario == "" || this.emailConfirm == "" || this.contraseniaUsuario == "" || this.contraseniaConfirm == "") {
+          alert("Por favor, rellene todos los campos.");
+        }
+        else {
+
+          if ((this.emailUsuario != this.emailConfirm) || (this.contraseniaUsuario != this.contraseniaConfirm)) {
+            alert("Comrpuebe que la los campos coincidan.");
+          }
+          else {
+            if (this.emailUsuario) {
+              if (!this.expresionEmail.test(this.emailUsuario)) {
+                alert("El formato del correo eléctronico no es válido.");
+              }
+              else {
+                alert("Se ha registrado correctamente.");
+              }
+            }
+
+          }
+
+
+
+        }
+
+    }
 }

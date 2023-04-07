c80587c4486bc5526f4303165d76c18c6a26bcae
Lian Salmerón López <lembajosa@gmail.com>
2023-04-07 16:52:14 +0200

    Arreglado buscador ingredientes

--- ETEN/src/app/app.module.ts
+++ ETEN/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-
+import { NgChartsModule } from 'ng2-charts';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,12 +12,10 @@ import { SignUpComponent } from './views/sign-up/sign-up.component';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutUsComponent } from './views/about-us/about-us.component';
-
 import { InfoRecetaComponent } from './views/info-receta/info-receta.component';
 import { EstadisticasAdminComponent } from './views/estadisticas-admin/estadisticas-admin.component';
 import { PerfilComponent } from './views/perfil/perfil.component';
 import { BuscadorCategoriaComponent } from './views/buscador-categoria/buscador-categoria.component';
-
 import { BuscadorIngredienteComponent } from './views/buscador-ingrediente/buscador-ingrediente.component';
 import { BuscadorTituloComponent } from './views/buscador-titulo/buscador-titulo.component';
 import { OfertasComponent } from './views/ofertas/ofertas.component';
@@ -32,17 +30,23 @@ import { OfertasComponent } from './views/ofertas/ofertas.component';
     LoginComponent,
     SignUpComponent,
     AboutUsComponent,
-
     InfoRecetaComponent,
     EstadisticasAdminComponent,
     PerfilComponent,
     BuscadorCategoriaComponent,
-
     BuscadorIngredienteComponent,
     BuscadorTituloComponent,
     OfertasComponent
-  ],
 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MdbCarouselModule,
+    NgbModule,
+    FormsModule,
+    NgChartsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

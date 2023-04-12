751d9bc3f231b4f28cd70c732c4de3b097e7507e
Dexter43 <svalerian.94@gmail.com>
2023-04-12 12:51:42 +0200

    filtrado de productos

--- ETEN/src/app/views/ofertas/ofertas.component.ts
+++ ETEN/src/app/views/ofertas/ofertas.component.ts
@@ -5,6 +5,7 @@ interface Product {
   title: string;
   description: string;
   price: number;
+  category: string;
 }
 
 @Component({
@@ -19,6 +20,9 @@ export class OfertasComponent implements OnInit {
   currentPage: number = 1;
   totalPages: number;
   pages: number[] = [];
+  categories: string[] = ['Productos Frescos', 'Despensa', 'Bebidas'];
+  selectedCategory: string = '';
+  filteredProducts: Product[] = [];
 
   constructor() {
     this.totalPages = 0;
@@ -26,30 +30,49 @@ export class OfertasComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.generateProducts();
+    this.filterProducts();
     this.totalPages = Math.ceil(this.products.length / this.itemsPerPage);
     this.setPage(this.currentPage);
     this.pages = Array(this.totalPages).fill(0).map((x, i) => i + 1);
   }
 
+  selectCategory(category: string): void {
+    this.selectedCategory = category;
+    this.filterProducts();
+    this.totalPages = Math.ceil(this.filteredProducts.length / this.itemsPerPage);
+    this.setPage(1); 
+    this.pages = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+  }
+  
   generateProducts(): Product[] {
     const products: Product[] = [];
     for (let i = 1; i <= 120; i++) {
+      const category = this.categories[Math.floor(Math.random() * this.categories.length)];
       const product: Product = {
         image: `https://via.placeholder.com/300x300?text=Product+${i}`,
         title: `Producto ${i}`,
         description: `Descripción del producto ${i}.`,
-        price: +(Math.random() * (10 - 1) + 1).toFixed(2)
+        price: +(Math.random() * (10 - 1) + 1).toFixed(2),
+        category: category
       };
       products.push(product);
     }
     return products;
   }
 
+  filterProducts(): void {
+    if (this.selectedCategory === '') {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
+    }
+  }
+
   setPage(page: number): void {
     this.currentPage = page;
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-    this.paginatedProducts = this.products.slice(startIndex, endIndex);
+    this.paginatedProducts = this.filteredProducts.slice(startIndex, endIndex); // Usa los productos filtrados
   }
 
   previousPage(): void {

b805a10518b0a8df0e29fc2f345b2d87dcc7c1f6
Lian Salmerón López <lembajosa@gmail.com>
2023-04-13 20:25:01 +0200

    Mejoras en el buscador

--- ETEN/src/app/views/buscador-ingrediente/buscador-ingrediente.component.ts
+++ ETEN/src/app/views/buscador-ingrediente/buscador-ingrediente.component.ts
@@ -9,13 +9,13 @@ export class BuscadorIngredienteComponent {
   MAX_TARJETAS = 5;
   ingredientes: string[] = [];
 
-  @ViewChild('nombreReceta') inputNombreReceta!: ElementRef<HTMLInputElement>;
-  @ViewChild('contenedorTarjetas') contenedorTarjetas!: ElementRef<HTMLElement>;
+  @ViewChild('nombreReceta', { static: true }) inputNombreReceta!: ElementRef<HTMLInputElement>;
+  @ViewChild('contenedorTarjetas', { static: true}) contenedorTarjetas!: ElementRef<HTMLElement>;
 
   agregarTarjeta() {
-    const nombreReceta = this.inputNombreReceta.nativeElement.value.trim();
+    const nombreReceta = (<HTMLInputElement>document.getElementById("nombreReceta")).value.trim();
     const nombreRecetaRecortado = nombreReceta.slice(0, 20) + (nombreReceta.length > 28 ? "..." + nombreReceta[nombreReceta.length - 1] : "");
-    
+
     if (!nombreRecetaRecortado) {
       alert("El campo de añadir ingrediente está vacío.");
     } else if (this.ingredientes.length >= this.MAX_TARJETAS) {

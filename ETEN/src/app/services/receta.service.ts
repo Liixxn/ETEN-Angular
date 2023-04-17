74dba8452185cbb9f948af560f15f9db37e8f9bb
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-04-17 18:16:59 +0200

    Buscador por categoría

---
+++ ETEN/src/app/services/receta.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Receta } from '../models/receta';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecetaService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public ObtenerTodasRecetas() {
+    return this.httpClient.post<Receta[]>("http://localhost:8000/api/recetas/ObtenerRecetas", null);
+  }
+}

b6dbf8122ad1203b9d481134aeea7a13ab0965e4
Lian Salmerón López <lembajosa@gmail.com>
2023-05-03 23:53:43 +0200

    Arreglo en la estructura de ofertas y paginación

--- ETEN/src/app/services/receta.service.ts
+++ ETEN/src/app/services/receta.service.ts
@@ -13,15 +13,9 @@ export class RecetaService {
     return this.httpClient.post<Receta[]>("http://localhost:8000/api/recetas/ObtenerRecetas", null);
   }
 
-  public ObtenerRecetasPorTitulo(titulo: string) {
-    return this.httpClient.get<Receta[]>("http://localhost:8000/api/recetas/BuscarReceta/" + titulo);
-
-  }
-
   public ObtenerUnaRecetas(idReceta: number) {
     return this.httpClient.get<Receta>("http://localhost:8000/api/recetas/ObtenerUnaReceta/" + idReceta);
   }
-
 
   public ObtenerRecetasPorCategoria(num_categoria: number) {
     return this.httpClient.get<Receta[]>("http://localhost:8000/api/recetas/ObtenerRecetasPorCategoria/" + num_categoria);
--- ETEN/src/app/views/ofertas/ofertas.component.ts
+++ ETEN/src/app/views/ofertas/ofertas.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Product {
-  image: string;
-  title: string;
-  description: string;
-  price: number;
-  category: string;
-}
 
 @Component({
   selector: 'app-ofertas',
@@ -14,76 +7,27 @@ interface Product {
   styleUrls: ['./ofertas.component.scss'],
 })
 export class OfertasComponent implements OnInit {
-  products: Product[] = [];
-  paginatedProducts: Product[] = [];
-  itemsPerPage: number = 20;
-  currentPage: number = 1;
-  totalPages: number;
-  pages: number[] = [];
-  categories: string[] = ['Productos Frescos', 'Despensa', 'Bebidas'];
-  selectedCategory: string = '';
-  filteredProducts: Product[] = [];
+
+  ofertas: string[] = [];
+
+  /* Paginacion */
+  currentIndex = -1;
+  page = 1;
+  count = 0;
+
 
   constructor() {
-    this.totalPages = 0;
+
   }
 
   ngOnInit(): void {
-    this.products = this.generateProducts();
-    this.filterProducts();
-    this.totalPages = Math.ceil(this.products.length / this.itemsPerPage);
-    this.setPage(this.currentPage);
-    this.pages = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+  this.ofertas.push("Oferta 1");
+    this.ofertas.push("Oferta 2");
+
   }
 
-  selectCategory(category: string): void {
-    this.selectedCategory = category;
-    this.filterProducts();
-    this.totalPages = Math.ceil(this.filteredProducts.length / this.itemsPerPage);
-    this.setPage(1); 
-    this.pages = Array(this.totalPages).fill(0).map((x, i) => i + 1);
-  }
-  
-  generateProducts(): Product[] {
-    const products: Product[] = [];
-    for (let i = 1; i <= 120; i++) {
-      const category = this.categories[Math.floor(Math.random() * this.categories.length)];
-      const product: Product = {
-        image: `https://via.placeholder.com/300x300?text=Product+${i}`,
-        title: `Producto ${i}`,
-        description: `Descripción del producto ${i}.`,
-        price: +(Math.random() * (10 - 1) + 1).toFixed(2),
-        category: category
-      };
-      products.push(product);
-    }
-    return products;
+  public handlePageChange(event: number): void {
+    this.page = event;
   }
 
-  filterProducts(): void {
-    if (this.selectedCategory === '') {
-      this.filteredProducts = this.products;
-    } else {
-      this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
-    }
-  }
-
-  setPage(page: number): void {
-    this.currentPage = page;
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    this.paginatedProducts = this.filteredProducts.slice(startIndex, endIndex); // Usa los productos filtrados
-  }
-
-  previousPage(): void {
-    if (this.currentPage > 1) {
-      this.setPage(this.currentPage - 1);
-    }
-  }
-
-  nextPage(): void {
-    if (this.currentPage < this.totalPages) {
-      this.setPage(this.currentPage + 1);
-    }
-  }
 }

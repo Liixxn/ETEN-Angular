fe34b1ebd5a7ca6c02de66eed465da0ecf65f4d1
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-05-24 14:02:38 +0200

    Llevar arriba al cambiar de pestaña

--- ETEN/src/app/app.component.ts
+++ ETEN/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { AutenticacionService } from './services/autenticacion.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,16 @@ export class AppComponent {
 
 
 
-  constructor(private autenticacionService: AutenticacionService) { }
+  constructor(private autenticacionService: AutenticacionService, private router: Router) { }
 
   ngOnInit() {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      window.scrollTo(0, 0);
     //this.modificarAdmin();
-  }
+  });
+    }
 
   public esAdmin() {
     return this.autenticacionService.isAdmin();

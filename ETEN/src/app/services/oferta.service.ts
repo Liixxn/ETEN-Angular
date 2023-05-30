837659abda4f008a3d930d839ceba6cb999e4347
Lian Salmerón López <lembajosa@gmail.com>
2023-05-30 21:56:00 +0200

    Borrado de función sin usar

--- ETEN/src/app/services/oferta.service.ts
+++ ETEN/src/app/services/oferta.service.ts
@@ -14,10 +14,6 @@ export class OfertaService {
     return this.httpClient.get<any[]>(environment.apiUrl + "ofertas/obtenerOfertasPorCategoria/" + num_categoria + "/" + pagina);
   }
 
-  public obtenerTodasOfertas() {
-    return this.httpClient.get<Oferta[]>(environment.apiUrl + "ofertas/ObtenerTodasOfertas");
-  }
-
   public sumarVisita(id_oferta: number) {
     return this.httpClient.get(environment.apiUrl + "ofertas/SumarVisitas/" + id_oferta);
   }
